1f0b7332a4a894a7e39e714360c9d2cac102055b
aidankennedy47 <aidankennedy47@gmail.com>
2025-06-20 02:01:14 +0000

    [automatic commit] 2025-06-20 02:01:14 UTC 122.150.237.193

--- part1/app.js
+++ part1/app.js
@@ -14,6 +14,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+let db;
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
