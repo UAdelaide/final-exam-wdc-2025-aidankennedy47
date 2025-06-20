3cf217a643983028a88e08749836db8b4ba6aae5
aidankennedy47 <aidankennedy47@gmail.com>
2025-06-20 02:16:25 +0000

    [automatic commit] 2025-06-20 02:16:25 UTC 122.150.237.130

--- part1/app.js
+++ part1/app.js
@@ -104,13 +104,17 @@ let db;
 
 
     // Insert data if table is empty
-    const [rows] = await db.execute('SELECT COUNT(*) AS count FROM books');
+    const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
     if (rows[0].count === 0) {
       await db.execute(`
-        INSERT INTO books (title, author) VALUES
-        ('1984', 'George Orwell'),
-        ('To Kill a Mockingbird', 'Harper Lee'),
-        ('Brave New World', 'Aldous Huxley')
+        INSERT INTO Users (username, email, password_hash, role)
+        VALUES
+        ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+        ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+        ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+        ('aidan', 'aidan@example.com', 'hashed999', 'walker'),
+        ('dave', 'dave@example.com', 'hashed000', 'owner');
+
       `);
     }
   } catch (err) {
