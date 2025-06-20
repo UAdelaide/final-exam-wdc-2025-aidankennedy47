7fa0ec6a309cc7809f300413eea323637d270b7c
aidankennedy47 <aidankennedy47@gmail.com>
2025-06-20 02:58:58 +0000

    [automatic commit] 2025-06-20 02:58:58 UTC 122.150.237.116

--- starthere/app.js
+++ starthere/app.js
@@ -61,10 +61,10 @@ let db;
 // Route to return books as JSON
 app.get('/api/dogs', async (req, res) => {
   try {
-    const [books] = await db.execute('SELECT * FROM books');
-    res.json(books);
+    const [dogs] = await db.execute('SELECT * FROM Dogs');
+    res.json(dogs);
   } catch (err) {
-    res.status(500).json({ error: 'Failed to fetch books' });
+    res.status(500).json({ error: 'Failed to fetch dogs' });
   }
 });
 
