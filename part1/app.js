2c1aed98cc63291191bfeb6f56d07bb37fbea39b
aidankennedy47 <aidankennedy47@gmail.com>
2025-06-20 02:48:10 +0000

    [automatic commit] 2025-06-20 02:48:10 UTC 122.150.237.149

--- part1/app.js
+++ part1/app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mysql = require('mysql2/promise');
-var db = require('db/db');
+// var db = require('db/db');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//let db;
+let db;
 
 (async () => {
   try {
