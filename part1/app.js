4b2e5f199d1f4880eba400d7986818fdbacac493
aidankennedy47 <aidankennedy47@gmail.com>
2025-06-20 02:32:12 +0000

    [automatic commit] 2025-06-20 02:32:12 UTC 122.150.237.193

--- part1/app.js
+++ part1/app.js
@@ -6,7 +6,7 @@ var mysql = require('mysql2/promise');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var apiRouter = require('./')
+var apiRouter = require('./routes/api');
 
 var app = express();
 
