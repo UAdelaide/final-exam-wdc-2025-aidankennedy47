3085afff8db919963a66b88e982058b7292e42d5
aidankennedy47 <aidankennedy47@gmail.com>
2025-06-20 03:05:34 +0000

    [automatic commit] 2025-06-20 03:05:34 UTC 122.150.237.193

--- part1/app.js
+++ part1/app.js
@@ -166,6 +166,30 @@ app.get('/api/dogs', async (req, res) => {
   }
 });
 
+// Route to return dogs as JSON
+app.get('/api/dogs', async (req, res) => {
+  try {
+    const [Dogs] = await db.execute(`SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username
+         FROM Dogs
+         JOIN Users ON Dogs.owner_id = Users.user_id`);
+    res.json(Dogs);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch Dogs' });
+  }
+});
+
+// Route to return dogs as JSON
+app.get('/api/dogs', async (req, res) => {
+  try {
+    const [Dogs] = await db.execute(`SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username
+         FROM Dogs
+         JOIN Users ON Dogs.owner_id = Users.user_id`);
+    res.json(Dogs);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch Dogs' });
+  }
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
