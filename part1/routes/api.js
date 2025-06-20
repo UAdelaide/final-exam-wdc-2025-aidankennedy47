352b05a070a75f6f4178cf1b19797994255fc20c
aidankennedy47 <aidankennedy47@gmail.com>
2025-06-20 02:42:35 +0000

    [automatic commit] 2025-06-20 02:42:35 UTC 122.150.237.193

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -5,7 +5,7 @@ var db = require('../db');
 /* GET users listing. */
 router.get('/dogs', async function(req, res, next) {
   try {
-    const [dogs] = await db.execute('SELECT * FROM Dogs');
+    const [dogs] = await db.execute('SELECT Dogs.name AS dog_name, Dogs.size');
     res.json(dogs);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch users' });
