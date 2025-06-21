fcc43d8a451f1f52c2bfde1074c7aec6891dfed4
aidankennedy47 <aidankennedy47@gmail.com>
2025-06-21 02:27:38 +0000

    [automatic commit] 2025-06-21 02:27:38 UTC 122.150.228.201

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -43,7 +43,7 @@ router.post('/login', async (req, res) => {
   const { username, password } = req.body;
   console.log(req.body);
   try {
-    //inserts username and password into database
+    //inserts username and password into Users table in database
     const [rows] = await db.query(`
       SELECT user_id, username, role FROM Users
       WHERE username = ? AND password_hash = ?
