cf353d9e9240e70aba50e1e95d48a61f21834513
aidankennedy47 <aidankennedy47@gmail.com>
2025-06-21 02:52:16 +0000

    [automatic commit] 2025-06-21 02:52:16 UTC 122.150.228.138

--- part2/routes/ownerRoutes.js
+++ part2/routes/ownerRoutes.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const db = require('../models/db');
 
 router.get('/dogs', async (req, res) => {
-  // gets owner
+  // gets owner_id from the session
   let owner_id = req.session.user.user_id;
   // console.log(owner_id);
 
   try {
+    // queries the database
     const [rows] = await db.query(`
       SELECT dog_id, name FROM Dogs WHERE owner_id = ?
     `, [owner_id]);
